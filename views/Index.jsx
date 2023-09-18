95e059bda71b99c128e86e3dba63df06dcf9fb5c
oksananaumova <oksana.naumova@mustpay.tech>
2023-09-18 13:06:27 -0400

    index.jsx view rendered at pokemon route

---
+++ views/Index.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+const pokemons = require("../models/pokemon");
+
+class MyFirstComponent extends React.Component {
+  render() {
+    
+    return (
+      <div>
+        <h1>See All The Pokemon!</h1>
+      </div>
+    );
+  }
+}
+module.exports = Index;
