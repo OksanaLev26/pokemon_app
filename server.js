5f7ff8cd9ec677cecabf72d9ac476d1e95ec8207
oksananaumova <oksana.naumova@mustpay.tech>
2023-09-18 11:50:18 -0400

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const jsxEngine = require("jsx-view-engine");
+const pokemons = require("./models/pokemon.js");
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+app.get("/", (req, res) => {
+    res.send('<h1>Welcome to the Pokemon App!</h1>');
+  });
+
+app.get("/pokemon/", (req, res) => {
+  res.render('Index', { pokemons: pokemons });
+});
+
+app.get("/pokemon/:indexOfPokemonArray", (req, res) => {
+  res.render("Show", {
+    pokemon: pokemons[req.params.indexOfPokemonArray],
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
