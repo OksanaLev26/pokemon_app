c5aab84ffbdc77e6b0753fff83706a5898fbd62c
oksananaumova <oksana.naumova@mustpay.tech>
2023-09-18 14:04:49 -0400

    Created show views of each pokemon

--- views/Index.jsx
+++ views/Index.jsx
@@ -2,23 +2,40 @@ const React = require("react");
 const pokemons = require("../models/pokemon");
 
 const myStyle = {
-    color: '#ffffff',
-    backgroundColor: '#000000',
-    };
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  padding: "20px",
+  color: "#ffffff",
+  backgroundColor: "#0f0b54",
+  height: "100vh",
+};
+
+const linkStyles = {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "25px",
+    margin: "20px",
+  };
+
+  const liStyles = {
+    marginBottom: "15px",
+  }
 
 class Index extends React.Component {
   render() {
-    
     return (
       <div style={myStyle}>
         <h1>See All The Pokemon!</h1>
         <ul>
           {pokemons.map((pokemon, i) => {
-            const name = `${pokemon.name[0].toUpperCase}${pokemon.name[0].slice(1)}`
+            const name = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(
+              1
+            )}`;
             return (
-              <li>
-                 <a href={`/pokemons/${i}`}>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</a>
-                
+              <li style={liStyles}>
+                <a style={linkStyles} href={`/pokemon/${i}`}>{name}</a>
               </li>
             );
           })}
---
+++ views/Show.jsx
@@ -0,0 +1,52 @@
+const React = require("react");
+const pokemons = require("../models/pokemon");
+
+const myStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  padding: "20px",
+  color: "#ffffff",
+  backgroundColor: "#0f0b54",
+  height: "100vh",
+};
+
+const buttonStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "200px",
+  height: "60px",
+  cursor: "pointer",
+  border: "1px solid white",
+  borderRadius: "8px",
+  color: "white",
+  marginTop: "40px",
+  fontSize: "28px",
+};
+
+const linkStyles = {
+  textDecoration: "none",
+  color: "white",
+};
+
+class Show extends React.Component {
+  render() {
+    const { pokemon } = this.props;
+    const name = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`;
+
+    return (
+      <div style={myStyle}>
+        <h1>Gotta Catch 'Em All</h1>
+        <h2>{name}</h2>
+        <img src={`${pokemon.img}.jpg`} />
+        <a style={linkStyles} href="/pokemon">
+          <div style={buttonStyles}>Back</div>
+        </a>
+      </div>
+    );
+  }
+}
+
+module.exports = Show;
